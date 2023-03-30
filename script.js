5f64f5a3270ac653e912686648c2a3962ec5c71d
Noel Ohaeri <noelohaeri@gmail.com>
2023-03-30 14:54:09 -0400

    Added more UI

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let grid = document.createElement("div");
 container.appendChild(grid);
 
 
-for (let i = 0; i < 496; i ++){
+let itemSize = document.getElementById("#container");
+
+for (let i = 0; i < 396; i ++){
     let items = document.createElement("div");
-    items.style.cssText = "width: 16px; height: 16px; display: inline-block"
+    items.style.cssText = "width: 16px; height: 16px; border: solid;"
     items.className = "item"
     items.addEventListener("mouseover", () => {items.style.backgroundColor = "black";}
 

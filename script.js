06a41c011c93791f909c9c110f6ca2f5a1a0dd70
Noel Ohaeri <noelohaeri@gmail.com>
2023-03-31 17:55:30 -0400

    Refined the look of the the project

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let container = document.querySelector(".container");
 container.style.gridTemplateColumns = "repeat(16, 1fr)"
+container.style.gridTemplateRows = "repeat(16, 1fr"
 
 
-for (let i = 0; i < 350; i ++){
+for (let i = 0; i < 256; i ++){
     const div = document.createElement("div");
     div.style.cssText = 'border: solid;'
     div.className = "item"
     div.addEventListener("mouseover", () => {div.style.backgroundColor = "black";}
 
     )
+    
     container.appendChild(div);
     
 }

8cb3a906b35fddd61e1e9e216e8cdd13a789c1da
Noel Ohaeri <noelohaeri@gmail.com>
2023-04-01 06:08:55 -0400

    Added more functionality. You can now change how precise you want to be able to draw

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 let container = document.querySelector(".container");
-container.style.gridTemplateColumns = "repeat(16, 1fr)"
-container.style.gridTemplateRows = "repeat(16, 1fr"
+let text = document.querySelector(".input");
+let enterBtn = document.querySelector(".selector");
+let clearBtn = document.querySelector(".clearBtn");
+
+clearBtn.addEventListener("click", clear)
 
 
-for (let i = 0; i < 256; i ++){
-    const div = document.createElement("div");
-    div.style.cssText = 'border: solid;'
-    div.className = "item"
-    div.addEventListener("mouseover", () => {div.style.backgroundColor = "black";}
+enterBtn.addEventListener("click", function(){
+   let allItems = document.querySelectorAll(".item");
 
-    )
+   for(let i=0; i < allItems.length; i++){
+    allItems[i].remove();
+   }
+
+makeGrid();
+   
+})
+
+function clear(){
+    let allItems = document.querySelectorAll(".item");
+
+    for(let i=0; i < allItems.length; i++){
+     allItems[i].remove();
+    }
+ 
+ makeGrid();
+}
+
+
+function makeGrid(){
+    container.style.gridTemplateColumns = `repeat(${text.value}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${text.value}, 1fr`;
+
+    for (let i = 0; i < text.value * text.value; i ++){
+        const div = document.createElement("div");
+        div.style.cssText = ''
+        div.className = "item"
+        div.addEventListener("mouseover", () => {div.style.backgroundColor = "black";}
     
-    container.appendChild(div);
+        )
+        
+        container.appendChild(div);
+        
+    }
     
 }
